0a1230ed90b2bb5ffe0acf0ba3faa4e1e10a0181
Pieter van Loon <git@pietervanloon.com>
2020-09-15 14:55:26 +0200

    Now ignorring git directory but showing other hidden directies from FZF

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,2 @@
 set -gx PATH $PATH ~/Git/depot_tools /Applications/MacVim.app/Contents/bin ~/go/bin /usr/local/bin/ /opt/local/bin /Volumes/case-sensitive/esp-open-sdk/xtensa-lx106-elf/bin /Applications/mCRL2.app/Contents/bin
-set -gx FLUTTER_FRAMEWORK_DIR $FLUTTER_FRAMEWORK_DIR /usr/local/flutter/bin/cache/artifacts/engine/ios 
+#set -gx FLUTTER_FRAMEWORK_DIR $FLUTTER_FRAMEWORK_DIR /usr/local/flutter/bin/cache/artifacts/engine/ios 
