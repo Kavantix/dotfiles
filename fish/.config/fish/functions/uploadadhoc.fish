471f0c290f69c6338259bc4462f5be8669826f53
Pieter van Loon <pharmit@pietervanloon.com>
2020-09-15 14:28:08 +0200

    Added fish config

---
+++ fish/.config/fish/functions/uploadadhoc.fish
@@ -0,0 +1,3 @@
+function uploadadhoc --description alias\ uploadadhoc\ scp\ /Users/pieter/Library/Mobile\\\ Documents/com\\\~apple\\\~CloudDocs/AppIpas/MedApp.ipa\ pieter@dev.pharmit.nl:/usr/share/pharmit/iostestingadhoc.pharmit.nl/MedApp.ipa
+	scp /Users/pieter/Library/Mobile\ Documents/com\~apple\~CloudDocs/AppIpas/MedApp.ipa pieter@dev.pharmit.nl:/usr/share/pharmit/iostestingadhoc.pharmit.nl/MedApp.ipa $argv;
+end
