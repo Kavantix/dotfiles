4f1e3f58c573cb03c2881dfb80a3d52bfc887500
Pieter van Loon <git@pietervanloon.com>
2021-07-31 10:27:52 +0200

    Updated packages

--- vim/.config/nvim/lua/kavantix/telescope.lua
+++ vim/.config/nvim/lua/kavantix/telescope.lua
@@ -2,7 +2,9 @@ local actions = require('telescope.actions')
 require('telescope').setup {
    defaults = {
       file_sorter = require('telescope.sorters').get_fzy_sorter,
-      prompt_position = 'top',
+      layout_config = {
+         prompt_position = 'top',
+      },
       sorting_strategy = 'ascending',
       prompt_prefix = ' >',
       color_devicons = true,
