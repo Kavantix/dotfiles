79630ab34db7910fa77c4b1a43fcfe5836e14c71
Pieter van Loon <git@pietervanloon.com>
2021-10-18 21:31:02 +0200

    Fixed glitch in kitty with lightline and added porth support

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,13 @@
 set -gx PATH $PATH ~/fvm/default/bin ~/.pub-cache/bin /usr/local/share ~/Git/depot_tools /Applications/MacVim.app/Contents/bin ~/go/bin /usr/local/bin/ /opt/local/bin /Volumes/case-sensitive/esp-open-sdk/xtensa-lx106-elf/bin /Applications/mCRL2.app/Contents/bin
+set -gx GO111MODULE on
 #set -gx FLUTTER_FRAMEWORK_DIR $FLUTTER_FRAMEWORK_DIR /usr/local/flutter/bin/cache/artifacts/engine/ios 
 # ghcup-env
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 test -f /Users/pieter/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/pieter/.ghcup/bin $PATH
 
 # NeoVim as manpager
-set -x MANPAGER "nvim -c 'set ft=man' -"
+# set -x MANPAGER "nvim -c 'set ft=man' -"
+set -x MANPAGER "bat"
 set -x EDITOR "nvim"
 
 #aliases
